56608df98965e1c8902a312086f7d4b6a87fc406
Fabian Friedrich <fabian.friedrich03@gmail.com>
2026-07-19 16:48:01 +0200

    Add UDP echo test and recompile binaries

--- test-scripts/udp-ping-pong-client.sh
+++ test-scripts/udp-ping-pong-client.sh
@@ -9,9 +9,12 @@ SERVER_PORT="${SERVER_PORT:-12345}"
 CLIENT_IP="${CLIENT_IP:-10.0.0.81}"
 CLIENT_PORT="${CLIENT_PORT:-12345}"
 
-echo "Starting ${LIBOS} UDP ping-pong client from ${CLIENT_IP}:${CLIENT_PORT}"
+CORE_ID="${CORE_ID:-3}"
+OS_TUNING="nice -n -20 chrt -f 99 taskset -c ${CORE_ID}"
+
+echo "Starting ${LIBOS} UDP ping-pong client from ${CLIENT_IP}:${CLIENT_PORT} (Pinned to core ${CORE_ID})"
 echo "Targeting server at ${SERVER_IP}:${SERVER_PORT}"
 echo "CONFIG_PATH=${CONFIG_PATH}"
 
-exec "${BUILD_DIR}/rust/udp-ping-pong.elf" \
+exec ${OS_TUNING} "${BUILD_DIR}/rust/udp-ping-pong.elf" \
   --client "${CLIENT_IP}:${CLIENT_PORT}" "${SERVER_IP}:${SERVER_PORT}"
